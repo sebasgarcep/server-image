0971b27eb59faafc30206ddaffcdcc1cf746a80d
Sebastian Garrido <sebasgarcep@gmail.com>
2024-02-18 20:00:41 -0500

    More work

--- packages/server-image-core/src/server/pure.ts
+++
@@ -1,9 +0,0 @@
-export * from "./caches/memoryCache";
-export * from "./transformers/pureTransformer";
-export * from "./resolvers/fetchResolver";
-export * from "./resolvers/kvResolver";
-export * from "./loaders";
-export * from "../utils";
-export * from "../types";
-// eslint-disable-next-line @typescript-eslint/no-inferrable-types
-export const version: string = "__remix_image_version";
