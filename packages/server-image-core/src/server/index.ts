2d52d3b78237ddc5f446e67867be4339dbb925c9
Sebastian Garrido <sebasgarcep@gmail.com>
2024-02-18 20:14:54 -0500

    More work

--- packages/server-image-core/src/server/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./resolvers";
-export * from "../utils";
-export * from "../types";
-// eslint-disable-next-line @typescript-eslint/no-inferrable-types
-export const version: string = "__remix_image_version";
