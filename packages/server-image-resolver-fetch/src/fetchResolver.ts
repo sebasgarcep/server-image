77fa59419aeab81302aaa26e37c2fa96c10fa5b6
Sebastian Garrido <sebasgarcep@gmail.com>
2024-02-19 00:59:13 -0500

    More work

--- packages/server-image-resolver-fetch/src/fetchResolver.ts
+++ packages/server-image-resolver-fetch/src/fetchResolver.ts
@@ -1,6 +1,4 @@
-import { RemixImageError } from "../../types/error";
-import { MimeType } from "../../types/file";
-import type { Resolver } from "../../types/resolver";
+import { MimeType, RemixImageError, Resolver } from "@sebasgarcep/server-image-core";
 
 export const fetchResolver: Resolver = async (_asset, url) => {
   const imgRequest = new Request(url, {
