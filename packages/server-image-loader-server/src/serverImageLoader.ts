c5c70b2cacdc39ada03a91db4ec1289a6b887871
Sebastian Garrido <sebasgarcep@gmail.com>
2024-02-19 00:38:52 -0500

    More work

--- packages/server-image-loader-server/src/serverImageLoader.ts
+++ packages/server-image-loader-server/src/serverImageLoader.ts
@@ -1,11 +1,6 @@
-import { ClientLoader } from "../types/client";
-import { encodeQuery } from "../utils/url";
+import { ClientLoader, encodeQuery } from "@sebasgarcep/server-image-core";
 
-export const serverImageLoader: ClientLoader = (
-  src,
-  loaderUrl,
-  loaderOptions
-) => {
+export const serverImageLoader: ClientLoader = (src, loaderUrl, loaderOptions) => {
   return encodeQuery(loaderUrl, {
     src: encodeURI(src),
     ...loaderOptions,
